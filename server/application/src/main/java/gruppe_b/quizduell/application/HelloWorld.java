2134c4adfbf6432364a31e215389b6e14346accb
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-10-09 16:10:23 +0200

    clean up and bring new projects to run

---
+++ server/application/src/main/java/gruppe_b/quizduell/application/HelloWorld.java
@@ -0,0 +1,7 @@
+package gruppe_b.quizduell.application;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello, World! from application layer");
+    }
+}
---
+++ server/domain/src/main/java/gruppe_b/quizduell/domain/entities/User.java
@@ -0,0 +1,6 @@
+package gruppe_b.quizduell.domain.entities;
+
+public class User {
+    public String id;
+    public String userName;
+}
