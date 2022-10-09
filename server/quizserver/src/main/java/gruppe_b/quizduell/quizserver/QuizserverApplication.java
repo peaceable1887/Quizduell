4cf03c6915c965efe81c291cbd6ff020ec9e9f82
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-10-09 15:09:38 +0200

    add quizserver project

---
+++ server/quizserver/src/main/java/gruppe_b/quizduell/quizserver/QuizserverApplication.java
@@ -0,0 +1,13 @@
+package gruppe_b.quizduell.quizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizserverApplication.class, args);
+	}
+
+}
---
+++ server/quizserver/src/test/java/gruppe_b/quizduell/quizserver/QuizserverApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe_b.quizduell.quizserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
