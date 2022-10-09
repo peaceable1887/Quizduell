8e2787fec3003e4479275f6e1c52a75babebfef6
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-10-09 15:08:05 +0200

    add lobbyserver project

---
+++ server/lobbyserver/src/main/java/gruppe_b/quizduell/lobbyserver/LobbyserverApplication.java
@@ -0,0 +1,13 @@
+package gruppe_b.quizduell.lobbyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyserverApplication.class, args);
+	}
+
+}
---
+++ server/lobbyserver/src/test/java/gruppe_b/quizduell/lobbyserver/LobbyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe_b.quizduell.lobbyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
