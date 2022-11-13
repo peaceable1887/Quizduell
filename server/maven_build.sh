87f5a3948bbd7c18b3fbeeaa5089353d751ab1a4
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-11-13 19:40:39 +0100

    add application to build script

--- server/maven_build.sh
+++ server/maven_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mvn install -DskipTests -f domain
+mvn install -DskipTests -f application
 mvn install -DskipTests -f common
 mvn install -DskipTests -f persistence
 mvn install -DskipTests -f authserver
