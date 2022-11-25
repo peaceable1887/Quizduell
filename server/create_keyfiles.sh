2d631ea1e415b4fa35e1b71c7f357d962f7a2555
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-11-25 19:59:32 +0100

    some fixes

--- server/create_keyfiles.sh
+++ server/create_keyfiles.sh
@@ -3,3 +3,5 @@
 openssl genrsa -out authserver/src/main/resources/certs/keypair.pem 2048
 openssl rsa -in authserver/src/main/resources/certs/keypair.pem -pubout -out authserver/src/main/resources/certs/public.pem
 openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in authserver/src/main/resources/certs/keypair.pem -out authserver/src/main/resources/certs/private.pem
+cp authserver/src/main/resources/certs/* lobbyserver/src/main/resources/certs/
+cp authserver/src/main/resources/certs/* quizserver/src/main/resources/certs/
