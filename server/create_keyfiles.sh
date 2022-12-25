ee7f3796771d69917c85568a36942f6b3ecd4c52
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-12-25 22:28:23 +0100

    update keyfile script

--- server/create_keyfiles.sh
+++ server/create_keyfiles.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+mkdir -p authserver/src/main/resources/certs
+mkdir -p lobbyserver/src/main/resources/certs
+mkdir -p quizserver/src/main/resources/certs
+mkdir -p statsserver/src/main/resources/certs
+
 openssl genrsa -out authserver/src/main/resources/certs/keypair.pem 2048
 openssl rsa -in authserver/src/main/resources/certs/keypair.pem -pubout -out authserver/src/main/resources/certs/public.pem
 openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in authserver/src/main/resources/certs/keypair.pem -out authserver/src/main/resources/certs/private.pem
+
 cp authserver/src/main/resources/certs/* lobbyserver/src/main/resources/certs/
 cp authserver/src/main/resources/certs/* quizserver/src/main/resources/certs/
+cp authserver/src/main/resources/certs/* statsserver/src/main/resources/certs/
