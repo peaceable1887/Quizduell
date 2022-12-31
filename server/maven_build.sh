89c9268d03409dc665b484fe47e6c1a3db594791
Christopher <christopher.burmeister@stud.th-luebeck.de>
2022-12-31 16:57:21 +0100

    update build script

--- server/maven_build.sh
+++ server/maven_build.sh
@@ -5,6 +5,7 @@ mvn install -DskipTests -f domain
 mvn install -DskipTests -f application
 mvn install -DskipTests -f common
 mvn install -DskipTests -f persistence
+mvn install -DskipTests -f infrastructure
 mvn install -DskipTests -f authserver
 mvn install -DskipTests -f lobbyserver
 mvn install -DskipTests -f quizserver
